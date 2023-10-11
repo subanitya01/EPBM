89545f246d8cecb0f953b257a6925339905bb2bc
Fikri <fikri@nrecc.gov.my>
2023-10-11 15:36:51 +0800

    Nama menu Keputusan Mesyuarat ditukar kepada Senarai Mesyuarat Tambahan menu baru Keputusan mesyuarat

---
+++ mesyuarat/keputusan.aspx
@@ -0,0 +1,129 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="keputusan.aspx.cs" Inherits="EPBM.mesyuarat.keputusan" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+	<h1 class="h3 mb-3">KEPUTUSAN <b>MESYUARAT</b></h1>
+	<asp:Panel ID="PanelNotFound" runat="server">
+		<div class="alert alert-info d-flex align-items-center w-100 alert-outline alert-dismissible" role="alert">
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			<div class="alert-icon me-3">
+				<i class="mt-n1" data-feather="info"></i>
+			</div>
+			<div class="alert-message">Tiada mesyuarat untuk diputuskan buat masa ini.</div>
+		</div>
+	</asp:Panel>
+	<asp:Panel ID="PanelFound" runat="server" Visible="false">
+		<asp:Panel ID="PanelComment" runat="server" Visible="false">
+			<div class="alert alert-warning d-flex align-items-center w-100 alert-outline alert-dismissible" role="alert">
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				<div class="alert-icon me-3">
+					<i class="mt-n1" data-feather="bell"></i>
+				</div>
+				<div class="alert-message"><asp:Literal ID="CatatanPengesahan" runat="server" /></div>
+			</div>
+		</asp:Panel>
+	<div class="card">
+		<div class="card-header pb-0">
+			<h5 class="card-title">MESYUARAT</h5>
+		</div>
+		<div class="card-body">
+			<asp:Panel ID="Panel1" runat="server">
+				<div class="input-group row">
+					<div class="col-md-6">
+					<asp:DropDownList ID="listMesyuarat" CssClass="form-select" runat="server" AutoPostBack="true" OnSelectedIndexChanged="RefreshPage" >  
+					</asp:DropDownList>
+					</div>
+					<div class="col-md-6">
+						<div class="btn-group btn-group-sm mb-3 float-end" role="group">
+							<asp:LinkButton ID="confirmBtn" CssClass="btn btn-info" data-bs-toggle="modal" data-bs-target="#meetingModal" runat="server">
+								<i class="mt-n1" data-feather="send"></i> Hantar Untuk Pengesahan
+							</asp:LinkButton>
+						</div>
+					</div>
+				</div>
+				
+				<div class="modal fade" id="meetingModal" tabindex="-1" aria-modal="true" role="dialog">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header bg-info">
+								<h5 class="modal-title text-white text-truncate">MESYUARAT <asp:Literal ID="TajukMesyuaratModal" runat="server"></asp:Literal></h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body m-3">
+								<p class="">Anda pasti hantar mesyuarat ini untuk kelulusan penyelia?</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+								<asp:LinkButton ID="sendBtn" runat="server" CssClass="btn btn-info" OnClick="BtnSend_Click">
+									Hantar
+								</asp:LinkButton>
+							</div>
+						</div>
+					</div>
+				</div>
+			</asp:Panel>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header pb-0">
+			<h5 class="card-title">SENARAI PERMOHONAN <asp:Literal ID="TajukPermohonan" runat="server" /></h5>
+		</div>
+		<div class="card-body table-responsive">
+			<asp:GridView 
+				ID="GridView1" 
+				runat="server" 
+				EmptyDataText="Tiada Rekod Dijumpai." 
+				ShowHeaderWhenEmpty="True" 
+				AutoGenerateColumns="False" 
+				OnRowDataBound="GridView1_OnRowDataBound" 
+				CssClass="table table-bordered table-striped table-hover">  
+
+				<Columns>
+					<asp:TemplateField HeaderText="#">
+						<ItemTemplate><%# Container.DataItemIndex + 1 %></ItemTemplate>
+					</asp:TemplateField>
+
+					<asp:BoundField DataField="TAJUK" HeaderText="TAJUK">
+					</asp:BoundField>
+
+					<asp:BoundField DataField="JABATAN" HeaderText="JABATAN" HeaderStyle-CssClass="text-center">
+					</asp:BoundField>
+
+					<asp:TemplateField HeaderText="STATUS" ItemStyle-CssClass="text-center" HeaderStyle-CssClass="text-center">
+						<ItemTemplate>
+							<asp:Label ID="lblStatus" CssClass="badge" runat="server"><%# Eval("STATUS") %></asp:Label>
+						</ItemTemplate>
+					</asp:TemplateField>
+
+					<asp:TemplateField HeaderText="KETERANGAN" HeaderStyle-CssClass="text-center">
+						<ItemTemplate>
+							<asp:Label ID="LblKeterangan" runat="server"><%# Eval("KETERANGAN").ToString().Replace(Environment.NewLine, "<br />") %></asp:Label>
+						
+							<asp:ListView runat="server"  ID="DetailsList">
+								<LayoutTemplate>
+									<ul class="list-group text-sm">
+										<li id="itemPlaceholder" runat="server" />
+									</ul>
+								</LayoutTemplate>
+								<ItemTemplate>
+									<li class="list-group-item p-1"><b><%#: Eval("Label") %>:</b> <%#: Eval("Text") %></li>
+								</ItemTemplate>
+							</asp:ListView>
+						</ItemTemplate>
+					</asp:TemplateField>
+
+					<asp:TemplateField ItemStyle-CssClass="text-center">
+						<ItemTemplate>
+							<a href="/keputusan/papar.aspx?id=<%# Eval("Id") %>&ReturnURL=<%# System.Web.HttpUtility.UrlEncode("/mesyuarat/pengesahan.aspx?id=" + Eval("IdMesyuarat")) %>" title="Papar"><i class="align-middle" data-feather="eye"></i></a>
+							<a href="/keputusan/edit.aspx?id=<%# Eval("Id") %>&ReturnURL=<%# System.Web.HttpUtility.UrlEncode("/mesyuarat/pengesahan.aspx?id=" + Eval("IdMesyuarat")) %>" class="text-secondary" title="Edit Keputusan"><i class="align-middle" data-feather="edit-2"></i></a>
+						
+						</ItemTemplate>
+					</asp:TemplateField>
+				</Columns>
+			</asp:GridView>
+		</div>
+	</div>
+	</asp:Panel>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder4" runat="server">
+</asp:Content>
---
+++ mesyuarat/keputusan.aspx.cs
@@ -0,0 +1,145 @@
+﻿using EPBM.permohonan;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EPBM.mesyuarat
+{
+    public partial class keputusan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                initMesyuaratList();
+            }
+        }
+
+        protected void initMesyuaratList()
+        {
+            string CommandText = "Select * from PaparMesyuarat WHERE IdStatusPengesahan<>4 ORDER BY Id";
+            DataTable dtMesyuarat = Utils.GetDataTable(CommandText);
+            ViewState["dtMesyuarat"] = dtMesyuarat;
+            //bool selected = false;
+            int id = Convert.ToInt32(Request.QueryString["id"]);
+
+            foreach (DataRow row in dtMesyuarat.Rows)
+            {
+                ListItem item = new ListItem(row["Mesyuarat"].ToString(), row["Id"].ToString());
+                //item.Value = row["Id"].ToString();
+                if (id == Convert.ToInt32(row["Id"]))
+                {
+                    item.Selected = true;
+                    //selected = true;
+                }
+                listMesyuarat.Items.Add(item);
+            }
+
+            if (dtMesyuarat.Rows.Count > 0)
+            {
+                BindData();
+                PanelFound.Visible = true;
+                PanelNotFound.Visible = false;
+            }
+        }
+        protected void BindData()
+        {
+            try
+            {
+                DataTable dtMesyuarat = (DataTable)ViewState["dtMesyuarat"];
+                TajukMesyuaratModal.Text = dtMesyuarat.Rows[0]["MESYUARAT"].ToString();
+                TajukPermohonan.Text = "BAGI MESYUARAT " + TajukMesyuaratModal.Text;
+                string CommandText2 = "Select Id, IdMesyuarat, Tajuk, CASE WHEN IdJabatan = 1 THEN NamaBahagian ELSE NamaJabatan END as Jabatan, IdStatusKeputusan, StatusKeputusan as STATUS, SyarikatBerjaya, Harga, Tempoh, AlasanKeputusan as KETERANGAN " +
+                                      "from Papar_Permohonan WHERE IdStatusPengesahan<>4 and IdMesyuarat=@Id and TarikhHapus IS NULL ORDER BY Id";
+                Dictionary<string, dynamic> queryParams2 = new Dictionary<string, dynamic>() { { "@Id", listMesyuarat.SelectedValue } };
+                DataTable dtPermohonan = Utils.GetDataTable(CommandText2, queryParams2);
+                GridView1.DataSource = dtPermohonan;
+                GridView1.DataBind();
+                int IdPengesahan = Convert.ToInt32(dtMesyuarat.Rows[0]["IdStatusPengesahan"]);
+                sendBtn.CommandArgument = listMesyuarat.SelectedValue;
+
+
+                if (IdPengesahan == 3)
+                {
+                    CatatanPengesahan.Text = dtMesyuarat.Rows[0]["CatatanPengesahan"].ToString().Replace(System.Environment.NewLine, "<br>");
+                    PanelComment.Visible = true;
+                }
+                if (IdPengesahan == 1 || IdPengesahan == 3)
+                {
+                    int JumlahMohon = Convert.ToInt32(dtMesyuarat.Rows[0]["JumlahPermohonan"]);
+                    int JumlahLulus = Convert.ToInt32(dtMesyuarat.Rows[0]["JumlahKelulusan"]);
+                    if (JumlahMohon < 1 || JumlahMohon != JumlahLulus)
+                    {
+                        confirmBtn.CssClass = confirmBtn.CssClass + " disabled";
+                        //confirmBtn.Attributes["title"] = "Sila lengkapkan maklumat keputusan bagi setiap permohonan";
+                        confirmBtn.Enabled = false;
+                        confirmBtn.Attributes.Remove("href");
+                        confirmBtn.OnClientClick = null;
+                    }
+                }
+            }
+            catch (Exception) { Utils.HttpNotFound(); }
+        }
+
+        protected void GridView1_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DataRowView drv = e.Row.DataItem as DataRowView;
+                Label lblStatus = e.Row.FindControl("lblStatus") as Label;
+                Label LblKeterangan = e.Row.FindControl("LblKeterangan") as Label;
+                ListView detailsList = e.Row.FindControl("DetailsList") as ListView;
+
+                if (drv.Row["IdStatusKeputusan"].ToString() == "1")
+                    lblStatus.CssClass = lblStatus.CssClass + " text-bg-info";
+                else if (drv.Row["IdStatusKeputusan"].ToString() == "2")
+                    lblStatus.CssClass = lblStatus.CssClass + " text-bg-success";
+                else
+                    lblStatus.CssClass = lblStatus.CssClass + " text-bg-danger";
+
+                if (drv.Row["IdStatusKeputusan"].ToString() == "3")
+                {
+                    detailsList.Visible = false;
+                }
+                else
+                {
+                    DataTable dt = new DataTable();
+                    dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Label"), new DataColumn("Text") });
+                    dt.Rows.Add("SYARIKAT BERJAYA", drv.Row["SyarikatBerjaya"].ToString());
+                    dt.Rows.Add("NILAI", "RM " + string.Format("{0:#,0.00}", drv.Row["Harga"]));
+                    dt.Rows.Add("TEMPOH", drv.Row["Tempoh"].ToString() + " BULAN");
+                    detailsList.DataSource = dt;
+                    detailsList.DataBind();
+                    LblKeterangan.Visible = false;
+                }
+            }
+        }
+        protected void RefreshPage(object sender, EventArgs e)
+        {
+            if (listMesyuarat.Items.Count > 0)
+            {
+                BindData();
+            }
+        }
+
+        protected void BtnSend_Click(object sender, EventArgs e)
+        {
+            DataTable dtrslt = ((DataTable)ViewState["dtMesyuarat"]);
+            int id = Convert.ToInt32(dtrslt.Rows[0]["Id"]);
+            int idStatus = Convert.ToInt32(dtrslt.Rows[0]["IdStatusPengesahan"]);
+            Dictionary<string, dynamic> queryParams = new Dictionary<string, dynamic>() { { "@Id", id } };
+
+            if (idStatus == 1 || idStatus == 3)
+            {
+                Utils.ExcuteQuery("UPDATE Mesyuarat SET IdStatusPengesahan = 2 WHERE Id = @Id", queryParams);
+                //Session["flash.success"] = "Mesyuarat telah dihantar untuk kelulusan penyelia!";
+                //Response.Redirect("/mesyuarat/senarai.aspx");
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "notyf.success('Mesyuarat telah dihantar untuk kelulusan penyelia!');", true);
+            }
+        }
+    }
+}
